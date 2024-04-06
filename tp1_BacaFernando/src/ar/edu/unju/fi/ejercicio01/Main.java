d60bc9268d4d27dafc0d993c461a4c67b7b350f9
fernando-eb2406 <feb-332@hotmail.com>
2024-04-05 22:34:34 -0300

    Agrego ejercicio01

---
+++ tp1_BacaFernando/src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.ejercicio01;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		//Declarar y asignar valores a cinco variables	
+		int num1 = 8;
+	    int num2 = 5;
+	    int num3 = 1;
+	    int num4 = 20;
+	    int num5 = 9;
+	    
+	    //Calcular el promedio
+	    double promedio = (num1 + num2 + num3 + num4 + num5) / 5.0;
+	    
+	    //Mostrar por consola
+	    System.out.println("El promedio de los valores " + num1 + ", " + num2 + ", " + num3 + ", " + num4 + ", " + num5 + " es: " + promedio);
+
+	}
+	
+}
