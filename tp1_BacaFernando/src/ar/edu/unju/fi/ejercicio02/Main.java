c54a09bf68c80c54ca4b8e639e015bb957a9c295
fernando-eb2406 <feb-332@hotmail.com>
2024-04-06 00:21:54 -0300

    Agrego ejercicio03

--- tp1_BacaFernando/src/ar/edu/unju/fi/ejercicio02/Main.java
+++ tp1_BacaFernando/src/ar/edu/unju/fi/ejercicio02/Main.java
@@ -12,11 +12,11 @@ public class Main {
 		Double cosenoDe05 = Math.cos(0.5);
 		
 		System.out.println("Nombre del país: " + nombrePais);
-        System.out.println("Edad de la persona: " + edadPersona + " años");
-        System.out.println("Altura del edificio: " + alturaEdificio + " metros");
-        System.out.println("Precio del producto: $" + precioProducto);
-        System.out.println("Número de teléfono: " + numeroTelefono);
-        System.out.println("Coseno de 0.5: " + cosenoDe05);
+		System.out.println("Edad de la persona: " + edadPersona + " años");
+		System.out.println("Altura del edificio: " + alturaEdificio + " metros");
+		System.out.println("Precio del producto: $" + precioProducto);
+		System.out.println("Número de teléfono: " + numeroTelefono);
+		System.out.println("Coseno de 0.5: " + cosenoDe05);
 		
 	}
 
---
+++ tp1_BacaFernando/src/ar/edu/unju/fi/ejercicio03/Main.java
@@ -0,0 +1,24 @@
+package ar.edu.unju.fi.ejercicio03;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese un número entero");
+		Integer numero = scanner.nextInt();
+		scanner.close();
+		
+		if (numero % 2 != 0	) {
+			Integer dobleDeNumero = numero * 2;
+			System.out.println("El doble del número ingresado es: " + dobleDeNumero);
+		} else {
+			Integer tripleDeNumero = numero * 3;
+			System.out.println("El triple del número ingresado es: " + tripleDeNumero);
+		}
+		
+	}
+
+}
